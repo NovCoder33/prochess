d15dce7f28106b41af21344e4eea647dd7aa21ea
SidR <68519263+SidCoderMan@users.noreply.github.com>
2025-10-30 18:20:22 -0500

    Quick commit, just changed port to render backend port

--- prochess/src/socket.ts
+++ prochess/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 //https://prochess-backend.onrender.com
-export const socket = io("http://localhost:8080", {
+export const socket = io("https://prochess-backend.onrender.com", {
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionAttempts: 15,
